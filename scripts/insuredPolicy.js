7128062a7902646b6c6ced53304e52de166d3218
Vera Ivanova <Vera.Ivanova@adacta-group.com>
2018-09-11 09:37:33 +0300

    lesson 3

--- scripts/insuredPolicy.js
+++ scripts/insuredPolicy.js
@@ -1,7 +1,22 @@
 var policy = {
-    insured: 1,
-    run: function(num, a) { console.log(a * (num + policy.insured)) }
+    policyHolder: {
+        name: 'Vera',
+        age: Math.ceil(Math.random() * 100)
+    },
+    coverage: {
+        basicTariff: Math.random() / 1000,
+        insuredSum: Math.random() * 100000
+    },
+
+    calculatePremium: function () {
+        var p = policy.coverage;
+        return Math.ceil(p.basicTariff * p.insuredSum);
+    },
+    printPolicy: function () {
+        var ph = policy.policyHolder;
+        var output = 'Policyholder (' + ph.name + ', ' + ph.age + ' years old) has a premium equals to ' + policy.calculatePremium() + ' USD.';
+        console.log(output);
+    }
 }
 
-policy.run(3, 5);
-
+policy.printPolicy();
